3c6e80ec0997647c115060575ee2d577f86109f3
Hieu Dinh <vanhieu@telio.vn>
2022-05-20 15:07:32 +0700

    build image with tag

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -3,6 +3,7 @@ set -xe
 
 echo This is cicd.sh ${KEY1}
 IMAGE=hieudv1408/sct-service:$(date +%s)
+echo ${IMAGE}
 docker build -t ${IMAGE} .
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker push hieudv1408/sct-service
+docker push ${IAMGE}
