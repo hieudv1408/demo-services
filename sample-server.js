4f26b312c6688ab1d1a32464268baecf785a427e
Hieu Dinh <vanhieu@telio.vn>
2022-05-20 10:08:04 +0700

    init

---
+++ sample-server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+var cors = require('cors');
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.listen(3001, () => {
+  console.log("Application started and Listening on port 3001");
+});
+
+app.get("/", async (req, res) => {
+  res.send({
+    serviceName: "zalo-store-service",
+    version: '1.0.0'
+  })
+});
