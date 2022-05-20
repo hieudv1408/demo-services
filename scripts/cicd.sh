597a0b38f774a60d4389edf07f1350d80bef12a9
Hieu Dinh <vanhieu@telio.vn>
2022-05-20 14:42:08 +0700

    build docker

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -2,3 +2,6 @@
 set -xe
 
 echo This is cicd.sh ${KEY1}
+docker build -t hieudv1408/sct-service .
+docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
+docker push hieudv1408/sct-service-dev
